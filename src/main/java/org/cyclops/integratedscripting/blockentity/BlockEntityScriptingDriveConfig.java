877005a3610f5870a2d0b0b9af85a7c4bb5d02d9
Ruben Taelman <rubensworks@gmail.com>
2024-06-22 11:17:14 +0200

    Fix event being registered to wrong mod

--- src/main/java/org/cyclops/integratedscripting/blockentity/BlockEntityScriptingDriveConfig.java
+++ src/main/java/org/cyclops/integratedscripting/blockentity/BlockEntityScriptingDriveConfig.java
@@ -3,7 +3,6 @@ package org.cyclops.integratedscripting.blockentity;
 import com.google.common.collect.Sets;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import org.cyclops.cyclopscore.config.extendedconfig.BlockEntityConfig;
-import org.cyclops.integrateddynamics.IntegratedDynamics;
 import org.cyclops.integratedscripting.IntegratedScripting;
 import org.cyclops.integratedscripting.RegistryEntries;
 
@@ -21,7 +20,7 @@ public class BlockEntityScriptingDriveConfig extends BlockEntityConfig<BlockEnti
                 (eConfig) -> new BlockEntityType<>(BlockEntityScriptingDrive::new,
                         Sets.newHashSet(RegistryEntries.BLOCK_SCRIPTING_DRIVE.get()), null)
         );
-        IntegratedDynamics._instance.getModEventBus().addListener(new BlockEntityScriptingDrive.CapabilityRegistrar(this::getInstance)::register);
+        IntegratedScripting._instance.getModEventBus().addListener(new BlockEntityScriptingDrive.CapabilityRegistrar(this::getInstance)::register);
     }
 
 }
