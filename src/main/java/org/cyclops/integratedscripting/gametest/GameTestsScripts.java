82cb854981cab4fb97f1be08b760212339af0951
Ruben Taelman <rubensworks@gmail.com>
2025-02-19 19:55:13 +0100

    Add game test for using item methods

--- src/main/java/org/cyclops/integratedscripting/gametest/GameTestsScripts.java
+++ src/main/java/org/cyclops/integratedscripting/gametest/GameTestsScripts.java
@@ -6,10 +6,13 @@ import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.gametest.GameTestHolder;
 import net.neoforged.neoforge.gametest.PrefixGameTestTemplate;
 import org.apache.commons.lang3.tuple.Pair;
 import org.cyclops.integrateddynamics.core.evaluate.operator.Operators;
+import org.cyclops.integrateddynamics.core.evaluate.variable.ValueObjectTypeItemStack;
+import org.cyclops.integrateddynamics.core.evaluate.variable.ValueTypeBoolean;
 import org.cyclops.integrateddynamics.core.evaluate.variable.ValueTypeInteger;
 import org.cyclops.integrateddynamics.core.evaluate.variable.ValueTypes;
 import org.cyclops.integrateddynamics.part.PartTypePanelDisplay;
@@ -80,6 +83,37 @@ public class GameTestsScripts {
 
         helper.succeedWhen(() -> {
             assertValueEqual(partAndState.getRight().getDisplayValue(), ValueTypeInteger.ValueInteger.of(11));
+        });
+    }
+
+    @GameTest(template = TEMPLATE_EMPTY, timeoutTicks = TIMEOUT)
+    public void testScriptsDisplayScriptOnItem(GameTestHelper helper) {
+        GameTestHelpersIntegratedScripting.NetworkPositions positions = createBasicNetwork(helper, POS);
+
+        // Write script
+        ScriptingNetworkHelpers.getScriptingData().setScript(positions.diskId(), Path.of("script0.js"), "function abc(item) { return item.isStackable() && item.size() >= 16; }", IScriptingData.ChangeLocation.MEMORY);
+
+        // Create variable from script
+        ItemStack variableScript = createVariableForScript(helper.getLevel(), positions.diskId(), Path.of("script0.js"), "abc");
+
+        // Create constants as input to the script's function
+        ItemStack variableConst1 = createVariableForValue(helper.getLevel(), ValueTypes.OBJECT_ITEMSTACK, ValueObjectTypeItemStack.ValueItemStack.of(new ItemStack(Items.APPLE, 32)));
+
+        // Insert all variables into the variable store
+        positions.variableStore().getInventory().setItem(0, variableScript);
+        positions.variableStore().getInventory().setItem(1, variableConst1);
+
+        // Create variable card for applying the function
+        ItemStack variableApplied = createVariableForOperator(helper.getLevel(), Operators.OPERATOR_APPLY, new int[]{
+                getVariableFacade(helper.getLevel(), variableScript).getId(),
+                getVariableFacade(helper.getLevel(), variableConst1).getId(),
+        });
+
+        // Place variable in display
+        Pair<PartTypePanelDisplay, PartTypePanelDisplay.State> partAndState = placeVariableInDisplayPanel(helper.getLevel(), positions.displayPanel(), variableApplied);
+
+        helper.succeedWhen(() -> {
+            assertValueEqual(partAndState.getRight().getDisplayValue(), ValueTypeBoolean.ValueBoolean.of(true));
         });
     }
 
